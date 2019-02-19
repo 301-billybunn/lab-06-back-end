22c2ca26fd11ac4c7264230ca6a87547d0550354
lizkavalski <lizkavalski@gmail.com>
2019-02-19 12:52:27 -0800

    started on the weather

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ app.get('/location', (request, response) => {
 });
 
 // Dark Sky route
+app.get('/weather', (request, response) => {
+    const weatherData = getWeather(request.query.data);
+    response.send(weatherData); 
+});
 
 // Yelp food review route
 
@@ -55,7 +59,7 @@ function handleError(err, res) {
     if (res) res.status(500).send('Sorry, something went worng');
 }
 
-// 
+// Location data handler function
 function searchtoLatLong(query) {
     const geoData = require('./data/geo.json');
     const location = new Location(query, geoData);
@@ -72,3 +76,26 @@ function Location(query, res) {
     this.longitude = res.results[0].geometry.location.lng;
 }
 
+// Weather data handler function
+function getWeather() {
+    const darkskyData = require('.data/darksky.json');
+    
+    // We are going to return an array of objects. We need to create that array.
+    const weatherSummaries = [];
+    // We need to iterate over our raw data
+    // Each object in the raw data should be passed through the constructor
+    // These new instances should be pushed into the array we just created
+
+    darkskyData.daily.data.forEach(day => {
+        weatherSummaries.push(new Weather(day));
+    })
+
+    // You need to return the array that has been filled with instances
+    return weatherSummaries;
+}
+
+// Constructs the weather object
+function Weather(day) {
+    this.forecast = day.summary;
+    this.time = new Date(day.time * 1000).toString().slice(0, 15);
+}
