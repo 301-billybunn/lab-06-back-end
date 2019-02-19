2cbe6c445d9e45cfe1e55e54ef15f48be0cc5c23
lizkavalski <lizkavalski@gmail.com>
2019-02-19 12:05:48 -0800

    add the cors and fixed the spelling error

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ require('dotenv').config();
 // Load express to do the heavy lifting for the server
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 // Establish the PORT number
 const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(cors());
 
 // Tell express where to load our "html" files from
 app.use(express.static('./public'));
@@ -31,9 +34,9 @@ app.get('/location', (request, response) => {
 // Hiking route
 
 // Path the redirects to index.html
-app.get('/', (request, response) => {
-  response.status(200).redirect('index.html');
-});
+// app.get('/', (request, response) => {
+//   response.status(200).redirect('index.html');
+// });
 
 // Add a catch-all to get routes that don't exist.
 // app.use('*', (request, response) => response.send(`Sorry, that route does not exist`));
@@ -53,6 +56,7 @@ function handleError(err, res) {
     if (res) res.status(500).send('Sorry, something went worng');
 }
 
+// 
 function searchtoLatLong(query) {
     const geoData = require('./data/geo.json');
     const location = new Location(query, geoData);
@@ -60,10 +64,11 @@ function searchtoLatLong(query) {
     return location;
 }
 
+// Constructs location object from API response
 function Location(query, res) {
     console.log('res in Location()', res);
     this.search_query = query;
-    this.formatted_query = res.reults[0].formatted_address;
+    this.formatted_query = res.results[0].formatted_address;
     this.latitude = res.results[0].geometry.location.lat;
     this.longitude = res.results[0].geometry.location.lng;
 }
